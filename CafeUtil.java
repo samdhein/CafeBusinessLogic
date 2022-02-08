a425799a6d6aa63f713e55670cd214766714fc98
samdhein <hein.sam@gmail.com>
2022-02-08 14:31:26 -0800

    clean up

--- CafeUtil.java
+++ CafeUtil.java
@@ -1,49 +1,9 @@
 import java.util.ArrayList;
 import java.util.Date;
 public class CafeUtil{
-    // different methods, like functions
-    public void printHello(){
-        System.out.println("Hello from CafeUtil");
-    }
 
     public String greetMessage(){
         return "Hello!";
-    }
-
-    public String greetSomeone(String name){
-        return String.format("Hello, %s",name);
-    }
-    public String greetSomeone(int age){
-        return String.format("Hello, you are "+ age + " years old.");
-    }
-    public String createMesage(String name, String petName, int age){
-        return String.format("Hello, I am %s. I like %s. He is %d years old.", name, petName, age);
-    }
-    public String getCurrentDate(){
-        Date d = new Date();
-        // option 1
-        String dStr = d.toString();
-        return dStr;
-        // option 2
-        // return ""+d    
-        // option 3
-        // String testStr = new Date().toString();
-        // return testStr;
-    }
-    // need to run a for loop to print array contents... loop through array
-    public void printIntArray(int[] numArr){
-        for(int i=0; i<numArr.length; i++){
-            System.out.println(numArr[i]);
-        }
-    }
-    // for loop (enhanced for loop shown in option 2, but no index)
-    public void printArrayList(ArrayList<String> strArr){
-        for(int i=0; i<strArr.size(); i++) {
-            System.out.println("Hashtag "+ i + " : " + strArr.get(i));
-        }
-        for(String element: strArr){
-            System.out.println(element);
-        }
     }
     int total = 0;
     public int getStreakGoal(int numWeeks){
